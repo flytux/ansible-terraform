970a5821a40e72fe6933a955fc67619e85466780
root <jaehoon.jung@kubeworks.net>
2023-09-09 12:39:54 +0000

    update: k3s upgrade

---
+++ refs/terraform-libvirt/deployment/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  ## Configuration options
+  uri = "qemu:///system"
+  #alias = "server2"
+  #uri   = "qemu+ssh://root@10.49.42.23/system"
+}
