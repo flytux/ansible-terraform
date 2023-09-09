93dc4c49683fa20d9754fa99305245c55ae03bf7
root <jaehoon.jung@kubeworks.net>
2023-09-09 12:43:03 +0000

    update: k3s upgrade

---
+++ kubeadm-libvirt/README.md
@@ -0,0 +1,14 @@
+# Requirements to be covered
+
+- Airgapped install
+- VM OS agnostic
+- Kubernetes version upgrade
+
+---
+### Current Status
+
+- install VMS
+- install containerd
+- install kubeadm, kubelet and related bianries
+- install container images
+- copy script to init, join and token
---
+++ kubeadm-libvirt/variables.tf
@@ -0,0 +1,15 @@
+variable "password" { default = "linux" }
+
+variable "ip_type" { default = "static" }
+
+variable "prefixIP" { default = "192.168.122" }
+
+variable "master_ip" { default = "192.168.122.311" }
+
+variable "dns_domain" { default = "kubeworks.net" }
+
+variable "diskPool" { default = "default" }
+
+variable "qemu_connect" { default = "qemu:///system" }
+
+
