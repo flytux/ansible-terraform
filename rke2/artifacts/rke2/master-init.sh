0241ab9469c90c6d6a6093305abd613b79c8e1cd
Jaehoon <hooney.jung@gmail.com>
2024-04-13 18:01:21 +0900

    Update master-init.sh

--- rke2/artifacts/rke2/master-init.sh
+++ rke2/artifacts/rke2/master-init.sh
@@ -3,7 +3,7 @@ export RKE2_ROOT=/root/rke2/v1.28.8
 mkdir -p /etc/rancher/rke2
 cp ./rke2/config.yaml /etc/rancher/rke2/
 
-#curl -sfL https://get.rke2.io | sh - 
+#curl -sfL https://get.rke2.io |  INSTALL_RKE2_VERSION=v1.28.8+rke2r1 sh -
 INSTALL_RKE2_ARTIFACT_PATH=$RKE2_ROOT sh $RKE2_ROOT/install.sh
 systemctl enable rke2-server.service --now
 
