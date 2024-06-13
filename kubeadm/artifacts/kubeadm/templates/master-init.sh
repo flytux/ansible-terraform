32cbe2b707407e6459f2613e6f6793bfefd997a0
root <argo@devops>
2024-06-13 19:31:23 +0900

    tested: ubuntu 22.04

--- kubeadm/artifacts/kubeadm/templates/master-init.sh
+++ kubeadm/artifacts/kubeadm/templates/master-init.sh
@@ -12,5 +12,5 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 
 # 03 install cni
 #kubectl create -f kubeadm/cni/calico.yaml
-cilium install --version 1.15.3
+#cilium install --version 1.15.3
 helm upgrade -i cilium ./kubeadm/cni/cilium-1.15.5.tgz -n kube-system
--- kvm/01-variables.tf
+++ kvm/01-variables.tf
@@ -12,13 +12,14 @@ variable "diskPool" { default = "default" }
 
 variable "qemu_connect" { default = "qemu:///system" }
 
-variable "cloud_image" { default = "focal-server-cloudimg-amd64.img" }
+variable "cloud_image" { default = "ubuntu-22.04-server-cloudimg-amd64.img" }
+#variable "cloud_image" { default = "focal-server-cloudimg-amd64.img" }
 
 variable "nodes" { 
          type = map(object({ role = string, octetIP = string , vcpu = number, memoryMB = number, incGB = number})) 
          default = {
-           master-01 = { role = "master",  octetIP = "11" , vcpu = 8, memoryMB = 1024 * 24,  incGB = 200},
-           worker-01 = { role = "worker",  octetIP = "21" , vcpu = 6, memoryMB = 1024 * 10,  incGB = 100},
-           worker-02 = { role = "worker",  octetIP = "22" , vcpu = 6, memoryMB = 1024 * 10, incGB = 100},
+           master-01 = { role = "master",  octetIP = "11" , vcpu = 4, memoryMB = 1024 * 8,  incGB = 200},
+           worker-01 = { role = "worker",  octetIP = "21" , vcpu = 4, memoryMB = 1024 * 10,  incGB = 100},
+           worker-02 = { role = "worker",  octetIP = "22" , vcpu = 4, memoryMB = 1024 * 10, incGB = 100},
   }
 }
